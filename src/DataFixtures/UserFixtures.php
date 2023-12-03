758d36ea4846070061c2697a4ed10e107413fd40
Michel <michel.recchia3@etu.univ-lorraine.fr>
2023-12-03 23:56:37 +0100

    ✨ feat: added authentication

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+
+class UserFixtures extends Fixture
+{
+
+    public const USER_REFERENCE = 'user_';
+
+    public function load(ObjectManager $manager): void
+    {
+        $Users = [
+            ["admin@gmail.com", "admin"]
+        ];
+        foreach ($Users as $key => $userCredentials) {
+            $user = new User();
+            $user->setEmail($userCredentials[0]);
+            $user->setPassword('$2y$13$z7ij./rHFIzOVHJL71i3tu0GIrGq5NZNp8txzHaEp/vOtPWdUnO6y');
+            $manager->persist($user);
+            $ref = self::USER_REFERENCE . $userCredentials[0];
+            $this->addReference($ref, $user);
+        }
+
+        $manager->flush();
+    }
+}
